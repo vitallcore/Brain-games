71cecbe5dbb041d460950fe2c6904434a69ec7aa
vitallcore <vitaslipknot@gmail.com>
2024-07-07 14:40:48 +0600

    modified the rest scripts separating them from game logic

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,38 +1,6 @@
-import random
+#!/usr/bin/env python3
 
-
-def is_even(n):
-    return n % 2 == 0
-
-
-def main():
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-    correct_answers_count = 0
-
-    while correct_answers_count < 3:
-        number = random.randint(1, 32)
-        print(f"Question: {number}")
-        answer = input("Your answer: ").strip().lower()
-
-        if is_even(number):
-            correct_answer = "yes"
-        else:
-            correct_answer = "no"
-
-        if answer == correct_answer:
-            print("Correct!")
-            correct_answers_count += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f"Congratulations, {name}!")
-
+from brain_games.games.brain_even import main
 
 if __name__ == "__main__":
     main()
