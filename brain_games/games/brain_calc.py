17f623000feea17b9a342c1949b2f45ce55c6b87
vitallcore <vitaslipknot@gmail.com>
2024-07-11 19:39:13 +0600

    corrected game files according to linter

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -44,7 +44,10 @@ def main():
             print("Correct!")
             correct_answers += 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(
+                f"'{user_answer}' is wrong answer ;(."
+                f" Correct answer was '{correct_answer}'."
+            )
             print(f"Let's try again, {name}!")
             return
 
